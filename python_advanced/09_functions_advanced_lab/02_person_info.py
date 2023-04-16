a924a5b59ee4b76acea49c26ed66b5474220eafd
Yavor Gornalov <y.gornalov@gmail.com>
2023-04-16 16:46:54 +0300

    02. Person Info added

---
+++ python_advanced/09_functions_advanced_lab/02_person_info.py
@@ -0,0 +1,10 @@
+# https://judge.softuni.org/Contests/Practice/Index/1838#1
+
+def get_info(**kwargs):
+    name = kwargs.get("name")
+    town = kwargs.get("town")
+    age = kwargs.get("age")
+    return f"This is {name} from {town} and he is {age} years old"
+
+
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
