1ce4a3d684058421c06921848bea312bfb5a8782
Yavor Gornalov <y.gornalov@gmail.com>
2023-04-24 18:24:23 +0300

    Console Connect Four basic logic implemented

--- python_advanced/15_workshop/console_connect_four.py
+++ python_advanced/15_workshop/console_connect_four.py
@@ -1,6 +1,7 @@
 ROWS, COLS = 6, 7
 PLAYER = {1: "@", 2: "#"}
 EMPTY = "-"
+WINNING_SEQUENCE = 3
 
 
 def init_playground(rows: int, cols: int):
@@ -16,7 +17,11 @@ def plot_playground(field):
 
 def player_move(field, player):
     rows, cols = len(field), len(field[0])
-    col = int(input(f"Player {player}, please a column: ")) - 1  # index
+    while True:
+        col = int(input(f"Player {player}, please a column: ")) - 1  # index
+        if 0 <= col < cols:
+            break
+        print("Please enter a valid column number!")
     for row in range(rows - 1, -1, -1):
         if field[row][col] == EMPTY:
             field[row][col] = PLAYER[player]
@@ -25,12 +30,82 @@ def player_move(field, player):
         print("This column is full!")
 
 
+def check_winning_combination(field, player, row, col):
+    rows, cols = len(field), len(field[0])
+
+    def check_up_down():
+        sequence = set()
+        r, c = row, col
+        while r >= 0 and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            r -= 1
+        r, c = row, col
+        while r < rows and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            r += 1
+        return len(sequence)
+
+    def check_left_right():
+        sequence = set()
+        r, c = row, col
+        while c >= 0 and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            c -= 1
+        r, c = row, col
+        while c < cols and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            c += 1
+        return len(sequence)
+
+    def check_left_diagonal():
+        sequence = set()
+        r, c = row, col
+        while r >= 0 and c >= 0 and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            r -= 1
+            c -= 1
+        r, c = row, col
+        while r < rows and c < cols and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            r += 1
+            c += 1
+        return len(sequence)
+
+    def check_right_diagonal():
+        sequence = set()
+        r, c = row, col
+        while r >= 0 and c >= 0 and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            r -= 1
+            c += 1
+        r, c = row, col
+        while r < rows and c < cols and field[r][c] == PLAYER[player]:
+            sequence.add((r, c))
+            r += 1
+            c -= 1
+        return len(sequence)
+
+    if any([
+        check_up_down() >= WINNING_SEQUENCE,
+        check_left_right() >= WINNING_SEQUENCE,
+        check_left_diagonal() >= WINNING_SEQUENCE,
+        check_right_diagonal() >= WINNING_SEQUENCE,
+    ]):
+        return player
+
+
 playground = init_playground(ROWS, COLS)
 plot_playground(playground)
 
 current_player, next_player = 1, 2
+winner = None
 while True:
-    player_move(playground, current_player)
-    plot_playground(playground)
+    result = player_move(playground, current_player)
+    if result:
+        plot_playground(playground)
+        winner = check_winning_combination(playground, current_player, result[0], result[1])
+    if winner:
+        print(f"Player {current_player} has won this game!")
+        break
 
     current_player, next_player = next_player, current_player
