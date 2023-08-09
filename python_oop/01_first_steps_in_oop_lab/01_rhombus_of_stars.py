a9fe9cd87de0602dc09e7df0a05c4b20a561968f
Yavor Gornalov <y.gornalov@gmail.com>
2023-08-09 22:46:59 +0300

    01_first_steps_in_oop_lab - solutions uploaded

---
+++ python_oop/01_first_steps_in_oop_lab/01_rhombus_of_stars.py
@@ -0,0 +1,19 @@
+# https://judge.softuni.org/Contests/Practice/Index/1934#0
+
+def print_rombus(size):
+    for star_count in range(1, size):
+        print_row(size, star_count)
+    for star_count in range(size, 0, -1):
+        print_row(size, star_count)
+
+
+def print_row(size, star_count):
+    for row in range(size - star_count):
+        print(" ", end="")
+    for row in range(1, star_count):
+        print("*", end=" ")
+    print("*")
+
+
+n = int(input())
+print_rombus(n)
---
+++ python_oop/01_first_steps_in_oop_lab/03_class_book.py
@@ -0,0 +1,13 @@
+# https://judge.softuni.org/Contests/Practice/Index/1934#2
+
+class Book:
+    def __init__(self, name, author, pages):
+        self.name = name
+        self.author = author
+        self.pages = pages
+
+
+book = Book("My Book", "Me", 200)
+print(book.name)
+print(book.author)
+print(book.pages)
