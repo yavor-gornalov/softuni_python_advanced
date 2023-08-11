68b6b033acfadd2e8f8b4e1aa7b34bd2bcdc7089
Yavor Gornalov <116191485+yavor-gornalov@users.noreply.github.com>
2023-08-11 10:50:36 +0300

    03_classes_and_objects_lab - uploaded

---
+++ python_oop/03_classes_and_objects_lab/01_vehicle.py
@@ -0,0 +1,15 @@
+# https://judge.softuni.org/Contests/Practice/Index/1936#0
+
+class Vehicle:
+    def __init__(self, mileage, max_speed=150):
+        self.mileage = mileage
+        self.max_speed = max_speed
+        self.gadgets = []
+
+
+car = Vehicle(20)
+print(car.max_speed)
+print(car.mileage)
+print(car.gadgets)
+car.gadgets.append('Hudly Wireless')
+print(car.gadgets)
