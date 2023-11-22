cdde630f174da398acca8b74c1e8e5a3d60800a1
Yavor Gornalov <y.gornalov@gmail.com>
2023-11-22 09:54:20 +0200

    11_python_oop_exam_11_december_2021/.../test.py -- updated

--- python_oop_exams/11_python_oop_exam_11_december_2021/03. Unit Testing_Skeleton/test/test.py
+++ python_oop_exams/11_python_oop_exam_11_december_2021/03. Unit Testing_Skeleton/test/test.py
@@ -5,11 +5,17 @@ from project.team import Team
 class TestTeam(TestCase):
     def setUp(self) -> None:
         self.team = Team("Dream")
-        self.second_team = Team("Team")
+        self.team.members = {"Peter": 21, "George": 23}
+
+        self.team2 = Team("Team")
+        self.team2.members = {"John": 22, "Tom": 23}
+
+        self.new_team = self.team + self.team2
 
     def test_constructor(self):
-        self.assertEqual("Dream", self.team.name)
-        self.assertEqual({}, self.team.members)
+        new_team = Team("NewTeam")
+        self.assertEqual("NewTeam", new_team.name)
+        self.assertEqual({}, new_team.members)
 
     def test_name_setter_with_non_letter_char_in_name(self):
         ve = [None] * 4
@@ -26,77 +32,78 @@ class TestTeam(TestCase):
         for error in ve:
             self.assertEqual(expected, str(error.exception))
 
-    def test_add_member_new_member(self):
-        result = self.team.add_member(**{"Ronaldo": 38, "Messi": 36})
-        expected = "Successfully added: Ronaldo, Messi"
+    def test_add_member_already_added(self):
+        result = self.team.add_member(**{"George": 22, "Peter": 23})
+        expected = "Successfully added: "
         self.assertEqual(result, expected)
-        self.assertEqual({"Ronaldo": 38, "Messi": 36}, self.team.members)
+        self.assertEqual({"Peter": 21, "George": 23}, self.team.members)
 
-    def test_add_new_member_and_existing_member(self):
-        self.team.add_member(**{"Ronaldo": 38, "Messi": 36})
-        result = self.team.add_member(**{"Messi": 36, "Levandovski": 34})
-        expected = "Successfully added: Levandovski"
+    def test_add_new_members(self):
+        result = self.team.add_member(**{"Peter": 23, "John": 22, "Tom": 23})
+        expected = "Successfully added: John, Tom"
         self.assertEqual(result, expected)
-        self.assertEqual({"Ronaldo": 38, "Messi": 36, "Levandovski": 34}, self.team.members)
+        self.assertEqual({"Peter": 21, "George": 23, "John": 22, "Tom": 23}, self.team.members)
 
     def test_remove_unknown_member(self):
-        self.team.add_member(**{"Ronaldo": 38, "Messi": 36})
-        result = self.team.remove_member("Levandovski")
-        expected = "Member with name Levandovski does not exist"
+        result = self.team.remove_member("Tom")
+        expected = "Member with name Tom does not exist"
         self.assertEqual(expected, result)
-        self.assertEqual({"Ronaldo": 38, "Messi": 36}, self.team.members)
 
     def test_remove_existing_member(self):
-        self.team.add_member(**{"Ronaldo": 38, "Messi": 36})
-        result = self.team.remove_member("Messi")
-        expected = "Member Messi removed"
+        result = self.team2.remove_member("Tom")
+        expected = "Member Tom removed"
         self.assertEqual(expected, result)
-        self.assertEqual({"Ronaldo": 38}, self.team.members)
 
-    def test_gt_dunder(self):
-        self.assertFalse(self.team.__gt__(self.second_team))
-        self.team.add_member(**{"Ronaldo": 38, "Messi": 36})
-        self.assertTrue(self.team > self.second_team)
-        self.assertFalse(self.team < self.second_team)
+    def test_gt_dunder_with_equal_team_members_count(self):
+        self.team2.remove_member("Tom")
+        self.assertEqual(True, self.team > self.team2)
+        self.assertEqual(False, self.team < self.team2)
 
     def test_len_dunder(self):
-        self.assertEqual(0, self.team.__len__())
-        self.assertEqual(0, self.second_team.__len__())
-
-        self.team.add_member(**{"Ronaldo": 38, "Messi": 36})
-
-        self.assertEqual(2, len(self.team))
-        self.assertEqual(0, len(self.second_team))
+        self.assertEqual(2, len(self.team2))
+        self.team2.remove_member("Tom")
+        self.assertEqual(1, len(self.team2))
+        self.team2.remove_member("John")
+        self.assertEqual(0, len(self.team2))
 
     def test_add_method(self):
-        self.team.add_member(Ronaldo=38)
-        self.second_team.add_member(Messi=36)
+        self.new_team = self.team + self.team2
+        self.assertEqual("DreamTeam", self.new_team.name)
+        self.assertEqual(4, len(self.new_team))
+        self.assertTrue(self.new_team > self.team)
+        self.assertEqual({"Peter": 21, "George": 23, "John": 22, "Tom": 23}, self.new_team.members)
 
-        dream_team = self.team + self.second_team
+    def test_add_method_with_same_members(self):
+        self.team3 = Team("TeamCopy")
+        self.team3.add_member(**{"Peter": 21, "George": 23})
+        self.new_team = self.team + self.team3
+        self.assertEqual("DreamTeamCopy", self.new_team.name)
+        self.assertEqual(2, len(self.new_team))
+        self.assertEqual({"Peter": 21, "George": 23}, self.new_team.members)
 
-        self.assertEqual("DreamTeam", dream_team.name)
-        self.assertEqual(2, len(dream_team))
-        self.assertEqual({"Ronaldo": 38, "Messi": 36}, dream_team.members)
+    def test_str_dunder(self):
+        result = str(self.new_team)
+        expected = """Team name: DreamTeam
+Member: George - 23-years old
+Member: Tom - 23-years old
+Member: John - 22-years old
+Member: Peter - 21-years old"""
+        self.assertEqual(expected, result)
+
+        self.new_team.add_member(**{"Adam": 23, "Evans": 21})
+        resul2 = str(self.new_team)
+        expected2 = """Team name: DreamTeam
+Member: Adam - 23-years old
+Member: George - 23-years old
+Member: Tom - 23-years old
+Member: John - 22-years old
+Member: Evans - 21-years old
+Member: Peter - 21-years old"""
+        self.assertEqual(expected2, resul2)
 
     def test_str_dunder_without_members(self):
-        result = str(self.team)
-        expected = "Team name: Dream"
-        self.assertEqual(expected, result)
-
-    def test_str_dunder_age_sort(self):
-        self.team.add_member(**{"Ronaldo": 38, "Messi": 36})
-        result = str(self.team)
-        expected = """Team name: Dream
-Member: Ronaldo - 38-years old
-Member: Messi - 36-years old"""
-        self.assertEqual(expected, result)
-
-    def test_str_dunder_name_sort(self):
-        self.team.add_member(**{"Ronaldo": 38, "Messi": 38})
-        result = self.team.__str__()
-        expected = """Team name: Dream
-Member: Messi - 38-years old
-Member: Ronaldo - 38-years old"""
+        result = str(Team("Empty"))
+        expected = "Team name: Empty"
         self.assertEqual(expected, result)
 
 
